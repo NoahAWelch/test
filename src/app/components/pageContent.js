3d129b09421c35954f1e6750d4d836133ada3331
NoahAWelch <148398477+NoahAWelch@users.noreply.github.com>
2023-11-06 15:45:49 -0800

    Delete src/app directory

--- src/app/components/pageContent.js
+++
@@ -1,4 +0,0 @@
-
-export default function PageContent() {
-  return <main className="m-2 bg-gray-200"></main>
-}
--- src/app/components/pageTitle.js
+++
@@ -1,3 +0,0 @@
-export default function PageTitle({ title }) {
-    return <h1 className="text-3xl bold m-2">{title}</h1>
-}
